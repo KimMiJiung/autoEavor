1f633131fc1f69f5010f6ff0546fe9bb206059f8
thenongboo <thenongboo@gmail.com>
2025-11-19 21:07:47 +0900

    메인화면1차 업데이트

---
+++ src/main/java/com/sbproject/autoEavor/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.sbproject.autoEavor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping(value= {"/main", "/"})
+public class MainController {
+
+	@GetMapping("")
+	public String main() {
+		return "main/main";
+	}
+	
+}
