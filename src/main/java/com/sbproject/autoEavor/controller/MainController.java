4b54087d1c6af1b1d455b7148679eee4909a2825
thenongboo <thenongboo@gmail.com>
2025-11-19 21:19:51 +0900

    메안화면 2 수정

--- src/main/java/com/sbproject/autoEavor/controller/MainController.java
+++ src/main/java/com/sbproject/autoEavor/controller/MainController.java
@@ -1,17 +1,13 @@
 package com.sbproject.autoEavor.controller;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
-=======
->>>>>>> 42b5e4db57e36675e63c47173d5942cc7447a0ff
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
-<<<<<<< HEAD
 @RequestMapping(value= {"/main", "/"})
 public class MainController {
 
@@ -20,15 +16,5 @@ public class MainController {
 		return "main/main";
 	}
 	
-=======
-@RequestMapping(value={"/", "/main"})
-public class MainController {
-	
-	@RequestMapping("")
-	public String main() {
-		return "/main/main";
-	}
-	
-	
->>>>>>> 42b5e4db57e36675e63c47173d5942cc7447a0ff
+
 }
